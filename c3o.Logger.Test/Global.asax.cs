ca868feb99b43335c74ebebf3b94df5ab0a17887
Fred Sobel <fred.sobel@gmail.com>
2016-03-28 22:41:55 -0500

    cleanup

--- c3o.Logger.Test/Global.asax.cs
+++ c3o.Logger.Test/Global.asax.cs
@@ -15,40 +15,5 @@ namespace c3o.Logger.Test
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
-
-
-        void ErrorLog_Logged(object sender, ErrorLoggedEventArgs args)
-        {
-            //args.Entry.Error.ApplicationName = "Test"   ;
-
-            //// send error to custom endpoint
-            //args.Entry.Error.Exception.Data["ElmahId"] = args.Entry.Id;
-            //var elmahIoLog = new Elmah.Io.ErrorLog(new Guid("da8ccd1a-da16-4ac6-a988-af6b506ab22d"), new Uri("http://localhost:59837/"));
-            
-            //elmahIoLog.Log(args.Entry.Error);
-
-   //         new Guid("da8ccd1a-da16-4ac6-a988-af6b506ab22d"), new Uri("http://localhost:59837/")
-        }
-
-        
-        void ErrorMail_Mailing(object sender, Elmah.ErrorMailEventArgs e)
-        {
-            int i = 0;
-            i++;
-
-        }
-
-        protected virtual void ErrorLog_Filtering(object sender, ExceptionFilterEventArgs e)
-        {
-            int i = 0;
-            i++;
-        }
-
-        protected virtual void ErrorMail_Filtering(object sender, ExceptionFilterEventArgs e)
-        {
-            int i = 0;
-            i++;
-        }
-
     }
 }
