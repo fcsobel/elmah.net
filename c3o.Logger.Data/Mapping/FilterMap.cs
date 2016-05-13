1d91aeb27f14724137ff64327f2d8d776a3cc4dd
Fred Sobel <fred.sobel@gmail.com>
2016-05-12 23:39:22 -0500

    cleanup

---
+++ c3o.Logger.Data/Mapping/FilterMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace c3o.Logger.Data
+{
+    public class FilterMap : EntityTypeConfiguration<Filter>
+    {
+        public FilterMap()
+        {
+            // Primary Key
+            this.HasKey(t => t.Id);
+
+            // Properties
+            this.Property(t => t.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            // Table & Column Mappings
+            this.ToTable("Filters");
+            this.Property(t => t.Id).HasColumnName("Id");
+            this.Property(t => t.Name).HasColumnName("Name");
+
+            // link to log
+            this.HasMany(c => c.FilterTypes)
+                .WithMany(x => x.Filters)
+                .Map(m =>
+                {
+                    m.MapLeftKey("FilterId");
+                    m.MapRightKey("TypeId");
+                    m.ToTable("FilterTypes");
+                });
+
+            // link to log
+            this.HasMany(c => c.FilterSources)
+                .WithMany(x => x.Filters)
+                .Map(m =>
+                {
+                    m.MapLeftKey("FilterId");
+                    m.MapRightKey("SourceId");
+                    m.ToTable("FilterSources");
+                });
+        }
+    }
+}
--- c3o.Logger.Data/Mapping/LogMap.cs
+++ c3o.Logger.Data/Mapping/LogMap.cs
@@ -23,49 +23,6 @@ namespace c3o.Logger.Data
             this.ToTable("Logs");
 			this.Property(t => t.Id).HasColumnName("Id");            
 			this.Property(t => t.Name).HasColumnName("Name");
-
         }
     }
-
-    public class FilterMap : EntityTypeConfiguration<Filter>
-    {
-        public FilterMap()
-        {
-            // Primary Key
-            this.HasKey(t => t.Id);
-
-            // Properties
-            this.Property(t => t.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-
-            // Table & Column Mappings
-            this.ToTable("Filters");
-            this.Property(t => t.Id).HasColumnName("Id");
-            this.Property(t => t.Name).HasColumnName("Name");
-
-            // link to log
-            this.HasMany(c => c.FilterTypes)
-                .WithMany(x=>x.Filters)
-                .Map(m =>
-                {
-                    m.MapLeftKey("FilterId");
-                    m.MapRightKey("TypeId");
-                    m.ToTable("FilterTypes");
-                });
-
-            // link to log
-            this.HasMany(c => c.FilterSources)
-                .WithMany(x => x.Filters)
-                .Map(m =>
-                {
-                    m.MapLeftKey("FilterId");
-                    m.MapRightKey("SourceId");
-                    m.ToTable("FilterSources");
-                });
-
-
-            //this.HasRequired(t => t.Log).WithMany(t => t.Applications).HasForeignKey(d => d.LogId);
-
-        }
-    }
-
 }
